c935f1b30508fc920950f51fc7462a0a2500e32e
PoojaDL <poojadl2002@gmail.com>
2023-11-03 18:31:25 +0530

    Added bodyParser to parse the request body

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use(bodyParser.urlencoded());
+
+app.use("/product-detail", (req, res, next) => {
   console.log("Inside a middleware");
-  res.send("<h1>Hello World</h1>");
-  next();
+  res.send(
+    '<form method="POST" action="/product"><input type="text" name="text"><input type="number" name="quantity"><button type="submit">Submit</button></form>'
+  );
 });
 
-app.use((req, res, next) => {
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>This is Home Page</h1>");
   console.log("Inside another middleware");
 });
 
